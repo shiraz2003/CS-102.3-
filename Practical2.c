64770c21b12a334aecba8a804562e4d237d93607
shiraz2003 <140880109+shiraz2003@users.noreply.github.com>
2023-07-29 13:46:45 +0530

    Add files via upload

---
+++ Practical2.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+
+
+/*
+Q1
+
+#include <stdio.h>
+int main()
+{
+int age;
+printf("HI, HOW OLD ARE YOU? ");
+scanf("%d",&age);
+printf("\n\n");
+printf("WELCOME %d \n",age);
+printf("LET'S BE FRIENDS \n");
+}
+
+*/
+
+/*
+Q2
+
+#include <stdio.h>
+int main()
+{
+printf("%15d%15d%15d\n", 2, 4, 8);
+printf("%15d%15d%15d\n", 3, 9, 27);
+printf("%15d%15d%15d\n", 4, 16, 64);
+}
+*/
+
+
+/*
+Q3
+
+#include <stdio.h>
+int main()
+{
+float avgspeed, distravelled,timetakn;
+printf("Enter the Distance Travelled: ");
+scanf("%f",&distravelled);
+printf("Enter the Time Taken: ");
+scanf("%f",&timetakn);
+avgspeed=distravelled/timetakn;
+printf("The Average Speed Taken is %f \n",avgspeed);
+}
+*/
+
+
+
+/*
+Q4
+
+#include <stdio.h>
+int main()
+{
+float C,F;
+printf("Enter the Temperature in Fahrenheit: ");
+scanf("%f",&F);
+C=5.0*(F-32.0)/ 9.0;
+printf("The Temperature in Celsius is %.2f \n",C);
+}
+
+}
+*/
