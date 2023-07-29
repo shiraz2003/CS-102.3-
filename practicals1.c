0efbc6c91e587b90bd0d8784d489458ad0a10d17
shiraz2003 <140880109+shiraz2003@users.noreply.github.com>
2023-07-29 13:32:50 +0530

    Add files via upload

---
+++ practicals1.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+ #include <stdio>
+int main()
+
+ /*
+Q1,
+  printf ("Name - Shiraz Sappideen\n");
+  printf ("School - Carey college");
+
+*/
+
+/*
+Q2.
+    printf(  "    *\n");
+    printf(  "  ***\n");
+    printf(  " *****\n");
+    printf(  "*******\n");
+
+*/
+
+/*
+Q3.
+  int intValue;
+  float floatValue;
+  char charValue[20];
+
+  printf("Enter an integer value: ");
+  scanf("%d", &intValue);
+
+  printf("Enter a floating-point value: ");
+  scanf("%f", &floatValue);
+
+  printf("Enter a character value: ");
+  scanf("%s", &charValue);
+
+  printf("Value of intValue: %d\n", intValue);
+  printf("Value of floatValue: %.2f\n", floatValue);
+  printf("Value of charValue: %s\n", charValue);
+
+
+/*
+Q4 .
+   int no1 , no2,total;
+  printf("enter no 1 ");
+  scanf ("%d",&no1);
+  printf ("enter no 2 ");
+  scanf ("%d", &no2);
+
+  total= no1+no2;
+  printf("total is %d " ,total);
+*/
+
+
+
+/*
+Q5.
+    float no1,no2,avg;
+
+    printf("enter no1 ");
+    scanf ("%f",&no1);
+    printf ("enter no2 ");
+    scanf ("%f",&no2);
+    avg = no1 + no2 /2;
+    printf("avg is %.1f",avg);
+*/
+
+
+/*
+Q6.
+    int byear, age;
+    printf("Enter your birth year: ");
+    scanf("%d", &byear);
+    age = 2023 - byear;
+    printf("Age: %d", age);
+    return 0;
+*/
+
+
+
+/*
+Q7.
+    printf("Enter number 1: ");
+    scanf("%d", &num1);
+
+    printf("Enter number 2: ");
+    scanf("%d", &num2);
+
+
+    printf("Before swap:\n");
+    printf("Number 1: %d\n", num1);
+    printf("Number 2: %d\n", num2);
+
+
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+
+    printf("After swap:\n");
+    printf("Number 1: %d\n", num1);
+    printf("Number 2: %d\n", num2);
+*/
+
+
+
+/*
+Q8
+The color: blue
+First number: 12345
+Second number: 0025
+Third number: 1234
+Float number: 3.14
+Hexadecimal: ff
+Octal: 377
+Unsigned value: 150
+  %.
+
+*/
